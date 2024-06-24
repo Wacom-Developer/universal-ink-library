dab507c265364db8b26ea3271905306eede4f2d9
Markus Weber <markus.weber@wacom.com>
2024-06-24 16:43:33 +0200

    Remove code to configure logger and improve documentation.

--- uim/codec/parser/uim.py
+++ uim/codec/parser/uim.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright © 2021-23 Wacom Authors. All Rights Reserved.
+# Copyright © 2021-present Wacom Authors. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import os
 from chunk import Chunk
 from io import BytesIO
 from pathlib import Path
-from typing import Tuple, Any, Optional
+from typing import Tuple, Any, Union
 
 from uim.codec.base import RIFF_HEADER, UIM_HEADER, HEAD_HEADER
 from uim.codec.parser.base import Parser, FormatException, SupportedFormats
@@ -28,25 +28,16 @@ from uim.codec.parser.decoder.decoder_3_1_0 import UIMDecoder310
 from uim.model.ink import InkModel
 
 # Create the Logger
-logger: Optional[logging.Logger] = None
-
-if logger is None:
-    logger: logging.Logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(ch)
-    logger.info('Completed configuring logger()!')
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 class UIMParser(Parser):
     """
-    Parser for Universal Ink Model data codec.
+    UIMParser
+    =========
+
+    Parser for Universal Ink Model data codec (UIM).
+    The parser is able to parse UIM files in version 3.0.0 and 3.1.0.
 
     Examples
     --------
@@ -65,6 +56,24 @@ class UIMParser(Parser):
 
     @staticmethod
     def __parse_version__(stream: BytesIO) -> Tuple[int, SupportedFormats]:
+        """
+        Parses the version of the UIM file.
+
+        Parameters
+        ----------
+        stream: BytesIO
+            Stream of the UIM file
+
+        Returns
+        -------
+        Tuple[int, SupportedFormats]
+            Size of the head and the supported format
+
+        Raises
+        ------
+        FormatException
+            Raises if the file is not an UIM file.
+        """
         head: bytes = stream.read(4)
         if head != UIM_HEADER:
             raise FormatException('Not an Universal Ink Model File.')
@@ -74,27 +83,34 @@ class UIMParser(Parser):
         version_major = int.from_bytes(stream.read(1), byteorder='big')
         version_minor = int.from_bytes(stream.read(1), byteorder='big')
         version_patch = int.from_bytes(stream.read(1), byteorder='big')
-        logger.debug('UIM Version: {}.{}.{}'.format(version_major, version_minor, version_patch))
+        logger.debug(f'UIM Version: {version_major}.{version_minor}.{version_patch}')
         if version_major == 3 and version_minor == 0 and version_patch == 0:
             return size_head, SupportedFormats.UIM_VERSION_3_0_0
-        elif version_major == 3 and version_minor == 1 and version_patch == 0:
+        if version_major == 3 and version_minor == 1 and version_patch == 0:
             return size_head, SupportedFormats.UIM_VERSION_3_1_0
         return size_head, SupportedFormats.NOT_SUPPORTED
 
     @classmethod
-    def parse_json(cls, path: Any) -> InkModel:
+    def parse_json(cls, path: Union[str, Path]) -> InkModel:
         """
         Parse ink file from either a `Path`, `str`.
 
         Parameters
         ----------
-        path: Any
+        path: Union[str, Path]
             Location of the JSON file
 
         Returns
         -------
            model - `InkModel`
                Parsed `InkModel` from UIM encoded stream
+
+        Raises
+        ------
+        ValueError
+            Raises if path is not a `str` or `Path`
+        FormatException
+            Raises if file does not exist.
         """
         if isinstance(path, (str, Path)):
             # Check if path does exist
@@ -108,20 +124,28 @@ class UIMParser(Parser):
                     logger.info('JSON decoder chosen.')
                     # Content parser
                     return UIMDecoder300.decode_json(fp)
+        raise ValueError(f"Unsupported format. Type must be str or Path, but is {type(path)}.")
 
-    def parse(self, path_or_stream: Any) -> InkModel:
+    def parse(self, path_or_stream: Union[str, bytes, memoryview, BytesIO, Path]) -> InkModel:
         """
         Parse the Universal Ink Model codec.
 
         Parameters
         ----------
-        path_or_stream: Any
+        path_or_stream: Union[str, bytes, memoryview, BytesIO, Path]
             `Path` of file, path as str, stream, or byte array.
 
         Returns
         -------
            model - `InkModel`
                Parsed `InkModel` from UIM encoded stream
+
+        Raises
+        ------
+        FormatException
+            Raises if the file is not an UIM file.
+        TypeError
+            Raises if the type is not supported.
         """
         if isinstance(path_or_stream, (str, Path)):
             # Check if path does exist
@@ -156,6 +180,6 @@ class UIMParser(Parser):
         size_head, version = UIMParser.__parse_version__(riff)
         if version == SupportedFormats.UIM_VERSION_3_0_0:
             return UIMDecoder300.decode(riff, size_head)
-        elif version == SupportedFormats.UIM_VERSION_3_1_0:
+        if version == SupportedFormats.UIM_VERSION_3_1_0:
             return UIMDecoder310.decode(riff, size_head)
         raise FormatException(f"Parser does not support this format. {version}")
