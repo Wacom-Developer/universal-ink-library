92d4b5844f43e3b47aa44856a4cd7fce04239be7
Markus Weber <markus.weber@wacom.com>
2024-09-25 12:55:23 +0200

    Update sample to use print util.

--- samples/sample_extract_math.py
+++ samples/sample_extract_math.py
@@ -13,56 +13,13 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from pathlib import Path
-from typing import List, Dict, Any, Tuple
+from typing import List, Dict, Any
 
 from uim.codec.parser.uim import UIMParser
+from uim.utils.print import print_tree
 from uim.model.helpers.schema_content_extractor import uim_schema_semantics_from
 from uim.model.ink import InkModel
 from uim.model.semantics.schema import CommonViews
-
-
-def build_tree(node_list: List[Dict[str, Any]]):
-    """
-    Build a tree structure from the node list.
-    Parameters
-    ----------
-    node_list: `List[Dict[str, Any]]`
-        List of nodes
-    """
-    # Step 1: Create dictionaries for nodes and parent-child relationships
-    children_dict: Dict[str, Any] = {}
-
-    for node in node_list:
-        parent_uri: str = node['parent_uri']
-        if parent_uri is not None:
-            if parent_uri not in children_dict:
-                children_dict[parent_uri] = []
-            children_dict[parent_uri].append(node)
-
-    # Step 2: Define a recursive function to print the tree
-    def print_tree(node_display: Dict[str, Any], indent: int = 0):
-        info: str = ""
-        attributes: List[Tuple[str, Any]] = node_display.get('attributes', [])
-        if "path_id" in node_display:
-            info = f"(#strokes:={len(node_display['path_id'])})"
-        elif "bounding_box" in node_display:
-            info = (f"(x:={node_display['bounding_box']['x']}, y:={node_display['bounding_box']['y']}, "
-                    f"width:={node_display['bounding_box']['width']}, "
-                    f"height:={node_display['bounding_box']['height']})")
-        print('|' + '-' * indent + f" [type:={node_display['type']}] - {info}")
-        if len(attributes) > 0:
-            print('|' + ' ' * (indent + 4) + "| -[Attributes:]")
-            for key, value in attributes:
-                print('|' + ' ' * (indent + 8) + f"\t|-- {key}:={value}")
-        if node_display['node_uri'] in children_dict:
-            for child in children_dict[node_display['node_uri']]:
-                print_tree(child, indent + 4)
-
-    # Step 3: Find the root node (where parent_uri is None) and start printing the tree
-    for node in node_list:
-        if node['parent_uri'] is None:
-            print_tree(node)
-
 
 if __name__ == '__main__':
     parser: UIMParser = UIMParser()
@@ -71,4 +28,4 @@ if __name__ == '__main__':
     math_structures: List[Dict[str, Any]] = uim_schema_semantics_from(ink_model,
                                                                       semantic_view=CommonViews.HWR_VIEW.value)
     # Print the tree structure
-    build_tree(math_structures)
+    print_tree(math_structures)
